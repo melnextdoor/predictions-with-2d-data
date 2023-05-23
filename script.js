9eba5d6e5a1e0d73289d4164659684c64e696d34
melnextdoor <mel.kj.chan@gmail.com>
2023-05-23 00:33:30 -0400

    Updates to create a scatterplot to visualize data; note we clean the data here and ensure that we have the data that we need before including into our dataset.

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('Hello TensorFlow');
+async function getData() {
+    const carsDataResponse = await fetch ('https://storage.googleapis.com/tfjs-tutorials/carsData.json')
+    const carsData = await carsDataResponse.json();
+
+    const cleanedCarsData = carsData.map(car => ({
+        mpg: car.Miles_per_Gallon,
+        horsepower: car.Horsepower,
+    })).filter(car => (car.mpg != null && car.horsepower != null))
+
+    return cleanedCarsData;
+}
+
+async function run() {
+    const data = await getData();
+    const values = data.map(d => ({
+        x: d.horsepower,
+        y: d.mpg,
+    }));
+
+    tfvis.render.scatterplot(
+        {name: 'Horsepower v MPG'},
+        {values},
+        {
+            xLabel: 'Horsepower',
+            ylabel: 'MPG',
+            height: 300
+        }
+    );
+}
+
+document.addEventListener('DOMContentLoaded', run);
